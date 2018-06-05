f05edea4079712be07606b0f11bc3177b8042664
rodrigo.uzcanga <rodrigo.uzcanga@saari.com.mx>
2018-06-05 14:04:09 -0700

    firma del home

--- WebAppTest/Startup.cs
+++ WebAppTest/Startup.cs
@@ -28,18 +28,18 @@ namespace WebAppTest
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            if (env.IsDevelopment())
-            {
-                app.UseDeveloperExceptionPage();
-                app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
-                {
-                    HotModuleReplacement = true
-                });
-            }
-            else
-            {
-                app.UseExceptionHandler("/Home/Error");
-            }
+            //if (env.IsDevelopment())
+            //{
+            //    app.UseDeveloperExceptionPage();
+            //    app.UseWebpackDevMiddleware(new WebpackDevMiddlewareOptions
+            //    {
+            //        HotModuleReplacement = true
+            //    });
+            //}
+            //else
+            //{
+            //    app.UseExceptionHandler("/Home/Error");
+            //}
 
             app.UseStaticFiles();
 
